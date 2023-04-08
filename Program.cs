ea8ba90b0648cb45651cf7ec61a998ebedc44635
Viktorya <79201287827@yandex.ru>
2023-04-08 21:18:00 +0300

    Задача 52

--- Program.cs
+++ Program.cs
@@ -2,11 +2,8 @@
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите размерность n массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] randomArray = new double[m,n];
-Console.Write("Введите позицию строки: ");
-int m2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите позицию столбца: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m,n];
+
 void mas(int m, int n)
 {
 int i,j;
@@ -16,15 +13,26 @@ for (i = 0; i < m; i++)
 Console.WriteLine();
 for (j = 0; j < n; j++)
 {
-randomArray[i,j] = rand.NextDouble();
-Console.Write($"{randomArray[i,j]:F2} ");
+randomArray[i,j] = rand.Next(1,9);
+Console.Write($"{randomArray[i,j]} ");
 }
 Console.WriteLine();
 }
 }
+void arif(int m, int n)
+{
+Console.Write("Среднее арифметическое каждого столбца: ");
+int i,j;
+Random rand = new Random();
+for (j = 0; j < n; j++)
+{
+double sum = 0;
+for (i = 0; i < m; i++)
+{
+sum = sum + randomArray[i,j];
+}
+Console.Write($"{sum/(i):F1}; ");
+}
+}
 mas(m,n);
-if (m2<1 || n2<1)
-Console.Write("Позиции строк не могут быть отрицательными");
-else if (m2 <= m+1 && n2 <= n+1)
-Console.Write($"Значение элемента равно {randomArray[m2-1,n2-1]:F2} ");
-else Console.Write("Такого элемента нет в массиве");
+arif(m,n);
